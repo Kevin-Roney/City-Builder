2fffedf784f27ee0ce07277acb19f082d50b549e
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-11 09:17:29 -0800

    Moved all DOM to JS and console logged to check accuracy

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 // import functions and grab DOM elements
+//images and counter
+const waterImg = document.getElementById('waterImage');
+const skylineImg = document.getElementById('skylineImage');
+const natureImg = document.getElementById('natureImage');
+const counter = document.getElementById('imageChangeCount');
+
+//city name
+const cityNm = document.getElementById('cityName');
+const cityNmInput = document.getElementById('cityNameInput');
+
+//dropdowns
+const waterDropdown = document.getElementById('waterSelector');
+const skylineDropdown = document.getElementById('skylineSelector');
+const natureDropdown = document.getElementById('natureSelector');
+
+//slogans
+const slgInput = document.getElementById('sloganInput');
+const slgButton = document.getElementById('sloganButton');
+const slg = document.getElementById('slogans');
+
 
 // let state
 
