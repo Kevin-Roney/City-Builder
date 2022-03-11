91b8a1542086e21e5156cfc7c067320ed66d0a30
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-11 10:36:20 -0800

    Finished all functions and event listeners in app.js, all functioning properly

--- app.js
+++ app.js
@@ -19,10 +19,56 @@ const slgInput = document.getElementById('sloganInput');
 const slgButton = document.getElementById('sloganButton');
 const slg = document.getElementById('slogans');
 
-
 // let state
+let waterCount = 0;
+let skylineCount = 0;
+let natureCount = 0;
 
+//slogans
+const slogans = [];
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+waterDropdown.addEventListener('change', () => {
+    const water = waterDropdown.value;
+    waterCount++;
+    waterImg.src = `./assets/${water}.jpg`;
+    displayCountStats();
+});
+skylineDropdown.addEventListener('change', () => {
+    const skyline = skylineDropdown.value;
+    skylineCount++;
+    skylineImg.src = `./assets/${skyline}.jpg`;
+    displayCountStats();
+});
+natureDropdown.addEventListener('change', () => {
+    const nature = natureDropdown.value;
+    natureCount++;
+    natureImg.src = `./assets/${nature}.jpg`;
+    displayCountStats();
+});
+slgButton.addEventListener('click', () => {
+    const newSlogan = slgInput.value;
+    slogans.push(newSlogan);
+    slgInput.value = '';
+    displaySlogan();
+});
+//city name input event listener goes here
+cityNmInput.addEventListener('input', () => {
+    const newCity = cityNmInput.value;
+    cityNm.textContent = newCity;
+});
+function displaySlogan() {
+    slg.textContent = '';
+    for (let slogan of slogans) {
+        const pTag = document.createElement('p');
+        pTag.classList.add('slogan');
+        pTag.textContent = slogan;
+        slg.append(pTag);
+    }
+}
+function displayCountStats() {
+    counter.textContent = `You have changed the head ${waterCount} times, the middle ${skylineCount} times and the bottom ${natureCount} times!`;
+}
